408f5b3ca830fbf14aa551608c94a8183d8a0924
南保 俊輔 <s2422051@stu.musashino-u.ac.jp>
2026-03-20 16:47:43 +0900

    TailwindCSSをCDN読み込みからビルド時コンパイルに変更

--- application/client/postcss.config.js
+++ application/client/postcss.config.js
@@ -1,8 +1,10 @@
 const postcssImport = require("postcss-import");
+const tailwindcss = require("@tailwindcss/postcss");
 const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
   plugins: [
+    tailwindcss(),
     postcssImport(),
     postcssPresetEnv({
       stage: 3,
